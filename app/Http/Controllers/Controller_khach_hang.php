c30ed050c211dac1b66f84104cd79039c61e1a3a
Tran <baotranno343@gmail.com>
2021-04-17 09:47:30 +0700

    17-04

--- app/Http/Controllers/Controller_khach_hang.php
+++ app/Http/Controllers/Controller_khach_hang.php
@@ -44,8 +44,17 @@ class Controller_khach_hang extends Controller
     }
     public function store(Request $request)
     {
-        //
+
         $input = $request->only(['email', 'sdt', 'mat_khau', 'ho_ten', 'dia_chi', 'chuc_vu']);
+        $check_kh = Model_khach_hang::where('email', $input['email'])->first();
+        $check_sdt = Model_khach_hang::where('sdt', $input['sdt'])->first();
+        if (!$check_kh) {
+            return response()->json(["status" => "error", "error" => "Email Đã Tồn Tại"]);
+        }
+        if (!$check_sdt) {
+            return response()->json(["status"  => "error", "error" => "SĐT Đã Tồn Tại"]);
+        }
+
         if ($file = $request->file('avatar')) {
 
             $name = uniqid() . "." . $file->extension();
